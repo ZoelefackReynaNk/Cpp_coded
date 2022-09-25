6af5572cc71c80b1523b1059b132b64b93b1fd16
Zoelefack Reyna Nkoumbouk <zoelefackreyna246@gmail.com>
2022-09-25 18:30:23 +0100

    Update Ticket_office.cpp

--- Ticket_office.cpp
+++ Ticket_office.cpp
@@ -1,1 +1,22 @@
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ages[5];
+    float res, min;
+    for (int i = 0; i < 5; ++i) {
+        cin >> ages[i];
+    }
+    min = ages[0];
+    for (int i= 0;i <= 4; i++){
+        //min = ages[0];
+        if (ages[i] < min){
+            min = ages[i];
+        }
+    }
+    res = 50 - (min*50)/100;
+    cout << res;
+    //cout << min;
+    return 0;
+}
